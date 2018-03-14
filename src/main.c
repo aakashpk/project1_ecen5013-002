0f01d72b4f86a046f6f1fe9677c74f5ab2307311
aakash <Aakash.Kumar@colorado.edu>
2018-03-13 23:50:53 -0600

    Main changed to prints sensor values continuously on BBG

--- src/main.c
+++ src/main.c
@@ -43,12 +43,16 @@ int main(int argc, char **argv)
     Create external request socket server
     */
 
+    /* TODO: Make an array of all thread functions and move the thread creating inside a loop
+     Parameters to be passed to the thread would be logger referrences,
+     those can also be put into an array */
+
     if(pthread_create(&threadIDs[0],NULL,socket_thread,NULL)!=0)
 		printf("Thread 1 creation failed\n");
 
     printf("Thread created with ID %ld\n",threadIDs[0]);
 
-    printf("Temp is %lf, light is %lf",get_temp(),get_light());
+    printf("Temp is %lf, light is %lf\n",get_temp(),get_light());
     
     pthread_join(threadIDs[0],NULL);
 
@@ -57,35 +61,16 @@ int main(int argc, char **argv)
     
     #ifdef BBB
     // Code that will execute only on BBB
-/*    printf("TLOW:%x, THIGH:%x\n",(read_reg_temp(TLOW)),read_reg_temp(THIGH));
 
-    int file;
-    uint8_t a=0x03;
-    uint16_t b=0xabcd;
-//    uint8_t c=0xef;
-  //  uint8_t d=0xab;
-    file=open(I2CBUSNAME,O_WRONLY);
-    ioctl(file,I2C_SLAVE,TEMP_SENSOR_ADDR);
-    printf("%d\n",write(file,&a,2));
-    //printf("%d : val: %x\n",write(file,&b,3),b);
-    printf("%d\n",write(file,&b,3));
-//    printf("%d\n",write(file,&d,3));
-    close(file);
-*/
+  
     light_sensor_init();
-    write_reg_light_word(THRESHLOW,(uint16_t)0xabcd);
-
-
-  //  while(1)
+    while(1)
     {
      
-    //for(uint8_t i=128;i<144;i++)
-        //printf("Reg 0x%x:0x%x ,",i,read_reg_light_byte(i));
+	    sensors_test_print();	
 
-    
     } 
     
-    printf("%x %x",read_reg_light_word(THRESHLOW),read_reg_light_word(THRESHHIGH));
-
     #endif
+
 }
