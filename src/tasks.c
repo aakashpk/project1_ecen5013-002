ee4f33d4dd0dd70953eae6e40586f739778c5ada
Miles Frain <miles.frain@colorado.edu>
2018-03-17 15:03:53 -0600

    Split queue response handling into each task type

--- src/tasks.c
+++ src/tasks.c
@@ -1,6 +1,6 @@
 /**
  * @brief Functions for thread tasks
- * 
+ *
  * @file tasks.c
  * @author Aakash Kumar
  * @author Miles Frain
@@ -17,40 +17,15 @@ char* data_header_type_strings[] = {
 
 logged_data_t * add_to_bdqueue(bdqueue * queue,data_header_type_t type)
 {
-    logged_data_t * msg;    
+    logged_data_t * msg;
     msg = (logged_data_t*)bdqueue_next_empty_request(queue);
     msg->type = type;
     msg->req_time=time(NULL);
     bdqueue_done_writing_request(queue);
-        
+
     msg = (logged_data_t*)bdqueue_next_response(queue, false);
     //bdqueue_done_reading_response(queue);
-    
-    return msg;
-}
 
-
-logged_data_t * bdqueue_response(bdqueue * queue,data_header_type_t type)
-{
-    logged_data_t * msg; 
-    msg = (logged_data_t*)bdqueue_next_request(queue, false);
-    msg->res_time=time(NULL); 
-
-    switch (type)
-    {
-        case HEARTBEAT:
-            msg->temperature.value=0;
-            break;
-        case TEMPERATURE:
-            msg->temperature.value=get_temp(0);
-            break;
-        case LIGHT:
-            msg->light.value=get_light();
-            break;
-    }
-
-    bdqueue_done_reading_request_and_writing_response(queue);
-    
     return msg;
 }
 
@@ -72,7 +47,7 @@ int thread_param_init(thread_param_t *param)
 int queue_init(bdqueue ** queue)
 {
     *queue=malloc(sizeof(bdqueue));
-    
+
     if(*queue==NULL) return -1;
 
     if (bdqueue_init(*queue, sizeof(logged_data_t), 5) == QUEUE_FAILURE)
@@ -87,30 +62,44 @@ int queue_init(bdqueue ** queue)
 
 void printQ(logged_data_t * msg)
 {
-    printf("req time:%ld resp time:%ld type:%s value:%lf \n",
-            msg->req_time,msg->res_time,data_header_type_strings[msg->type],msg->light.value);
+    printf("%p req time:%ld resp time:%ld type:%s value:%lf \n",
+            msg, msg->req_time,msg->res_time,data_header_type_strings[msg->type],msg->light.value);
 }
 
 
 void * temperature_task(void * thread_param)
 {
-    
+
     #ifdef BBB
     if(temp_sensor_init()==0) LOG_STR("Temp Sensor Self Test Done\n");
-    else 
+    else
     {
         LOG_STR("Temp Sensor Init Failed\n");
         exit(1);
     }
     #endif
-    
+
     thread_param_t * p1= (thread_param_t*)thread_param;
-        
+
     while(p1->keep_thread_alive)
     {
-        
-        bdqueue_response(p1->temp_q,TEMPERATURE);
+        logged_data_t *msg = (logged_data_t*)bdqueue_next_request(p1->temp_q, false);
+        msg->res_time=time(NULL);
 
+        switch (msg->type)
+        {
+            case HEARTBEAT:
+                msg->temperature.value = 0;
+                break;
+            case TEMPERATURE:
+                msg->temperature.value = get_temp(0); // TODO use enumerated type
+                break;
+            default:
+                abort(); // wrong queue
+                break;
+        }
+
+        bdqueue_done_reading_request_and_writing_response(p1->temp_q);
     }
 
     bdqueue_destroy(p1->temp_q);
@@ -120,26 +109,41 @@ void * temperature_task(void * thread_param)
 
 void * light_task(void * thread_param)
 {
-    
+
     #ifdef BBB
     if(light_sensor_init()==0) LOG_STR("Light Sensor Self Test Done\n");
-    else 
+    else
     {
         LOG_STR("Light Sensor Init Failed\n");
         exit(1);
     }
     #endif
-    
+
     thread_param_t * p1= (thread_param_t*)thread_param;
-    
+
     while(p1->keep_thread_alive)
     {
-        bdqueue_response(p1->light_q,LIGHT);
-        
+        logged_data_t *msg = (logged_data_t*)bdqueue_next_request(p1->light_q, false);
+        msg->res_time=time(NULL);
+
+        switch (msg->type)
+        {
+            case HEARTBEAT:
+                msg->light.value = 0;
+                break;
+            case LIGHT:
+                msg->light.value = get_light();
+                break;
+            default:
+                abort(); // wrong queue
+                break;
+        }
+
+        bdqueue_done_reading_request_and_writing_response(p1->light_q);
     }
-    
+
      bdqueue_destroy(p1->light_q);
-    
+
     return NULL;
 
 }
